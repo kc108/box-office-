20e30122690ac23b39e04800add1c85394787b24
Kim Carpico <kcarpico@gmail.com>
2021-09-20 15:23:40 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return(
-    <div>
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is the HomePage
+      </Route>
+
+      <Route path="/starred">
+        This is starred
+      </Route>
+
+      <Route>
+        This is 404 Page
+      </Route>
+    </Switch>
   );
 };
 
